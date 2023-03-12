17dceb528aec1bd7fe7c2232b5d3d821ecbb9c96
Oleg Astashin <95183292+ophlop@users.noreply.github.com>
2023-03-12 23:53:12 +0400

    -- script show noResult message when we have error

--- src/script/index.js
+++ src/script/index.js
@@ -94,94 +94,100 @@ window.onload = async function () {
 
 function resetInput() {
     document.querySelector(".searchCity").value = ""
+    document.querySelector(".noResult").classList.add("noneActive")
 }
 
 async function fetchData() {
     let city = document.querySelector(".searchCity").value;
 
     const result = await fetch(`${link}${city}`, options);
-    const data = await result.json();
+        if (result.ok) {
+            const data = await result.json();
+            console.log(data);
 
-    console.log(data);
+            document.querySelector(".noResult").classList.add("noneActive")
 
-    let iconWeather = document.querySelector(".icon-weather");
-    let icon = `${data.current.condition.icon}`;
-    let rightIcon =
-        data.current.is_day == 1
-            ? (iconWeather.src = `./src/image/day/${icon.slice(
-                  icon.length - 7
-              )}`)
-            : (iconWeather.src = `./src/image/night/${icon.slice(
-                  icon.length - 7
-              )}`);
+            let iconWeather = document.querySelector(".icon-weather");
+            let icon = `${data.current.condition.icon}`;
+            let rightIcon =
+                data.current.is_day == 1
+                    ? (iconWeather.src = `./src/image/day/${icon.slice(
+                        icon.length - 7
+                    )}`)
+                    : (iconWeather.src = `./src/image/night/${icon.slice(
+                        icon.length - 7
+                    )}`);
 
-    let currentCity = document.querySelector(".city_current");
-    currentCity.innerHTML = `${data.location.name}`;
+            let currentCity = document.querySelector(".city_current");
+            currentCity.innerHTML = `${data.location.name}`;
 
-    let currentCountry = document.querySelector(".country_current");
-    currentCountry.innerHTML = `${data.location.country}`;
+            let currentCountry = document.querySelector(".country_current");
+            currentCountry.innerHTML = `${data.location.country}`;
 
-    let currentTemp = document.querySelector(".temp_c");
-    let rightTemp = `${data.current.temp_c}`;
-    let tempCurrent = (currentTemp.innerHTML =
-        `${rightTemp[0]}` == "-" ? `${rightTemp} °С` : `+${rightTemp} °С`);
+            let currentTemp = document.querySelector(".temp_c");
+            let rightTemp = `${data.current.temp_c}`;
+            let tempCurrent = (currentTemp.innerHTML =
+                `${rightTemp[0]}` == "-" ? `${rightTemp} °С` : `+${rightTemp} °С`);
 
-    let currentFeelsTemp = document.querySelector(".feelslike_c");
-    let rightFeelsTemp = `${data.current.feelslike_c}`;
-    currentFeelsTemp.innerHTML =
-        `${rightFeelsTemp[0]}` == "-"
-            ? `Feels Like: ${rightFeelsTemp} ° C`
-            : `Feels Like: +${rightFeelsTemp} °С`;
+            let currentFeelsTemp = document.querySelector(".feelslike_c");
+            let rightFeelsTemp = `${data.current.feelslike_c}`;
+            currentFeelsTemp.innerHTML =
+                `${rightFeelsTemp[0]}` == "-"
+                    ? `Feels Like: ${rightFeelsTemp} ° C`
+                    : `Feels Like: +${rightFeelsTemp} °С`;
 
-    let weatherText = document.querySelector(".text-weather");
-    weatherText.innerHTML = `${data.current.condition.text}`;
+            let weatherText = document.querySelector(".text-weather");
+            weatherText.innerHTML = `${data.current.condition.text}`;
 
-    let currentTime = document.querySelector(".lastUpdate");
-    currentTime.innerHTML = `Last Update: ${data.current.last_updated}`;
+            let currentTime = document.querySelector(".lastUpdate");
+            currentTime.innerHTML = `Last Update: ${data.current.last_updated}`;
 
-    let appIcon = document.querySelector(".icon-header");
-    appIcon.append(iconWeather, weatherText);
+            let appIcon = document.querySelector(".icon-header");
+            appIcon.append(iconWeather, weatherText);
 
-    let appFirstData = document.querySelector(".titleInfo_city");
-    appFirstData.append(currentCity, currentCountry);
+            let appFirstData = document.querySelector(".titleInfo_city");
+            appFirstData.append(currentCity, currentCountry);
 
-    let appTempData = document.querySelector(".titleInfo_temp");
-    appTempData.append(currentTemp, currentFeelsTemp);
+            let appTempData = document.querySelector(".titleInfo_temp");
+            appTempData.append(currentTemp, currentFeelsTemp);
 
-    let appUpdateData = document.querySelector(".titleInfo_time");
-    appUpdateData.append(currentTime);
+            let appUpdateData = document.querySelector(".titleInfo_time");
+            appUpdateData.append(currentTime);
 
-    let currentHumidity = document.querySelector(".humidity");
-    currentHumidity.innerHTML = `${data.current.humidity}%`;
+            let currentHumidity = document.querySelector(".humidity");
+            currentHumidity.innerHTML = `${data.current.humidity}%`;
 
-    let currentCloud = document.querySelector(".cloud");
-    let cloud = data.current.cloud;
-    currentCloud.innerHTML = `${cloud}%(${
-        0 < cloud && cloud < 25
-            ? "clear sky"
-            : 25 <= cloud && cloud < 50
-            ? "partly cloudy"
-            : 50 <= cloud && cloud < 75
-            ? "overcast"
-            : "cloudy"
-    })`;
+            let currentCloud = document.querySelector(".cloud");
+            let cloud = data.current.cloud;
+            currentCloud.innerHTML = `${cloud}%(${
+                0 < cloud && cloud < 25
+                    ? "clear sky"
+                    : 25 <= cloud && cloud < 50
+                    ? "partly cloudy"
+                    : 50 <= cloud && cloud < 75
+                    ? "overcast"
+                    : "cloudy"
+            })`;
 
-    let currentWind = document.querySelector(".wind_kph");
-    currentWind.innerHTML = `${data.current.wind_kph} kph`;
+            let currentWind = document.querySelector(".wind_kph");
+            currentWind.innerHTML = `${data.current.wind_kph} kph`;
 
-    let appHumidity = document.querySelector(".app_humidity-data");
-    appHumidity.append(currentHumidity);
+            let appHumidity = document.querySelector(".app_humidity-data");
+            appHumidity.append(currentHumidity);
 
-    let appCloud = document.querySelector(".app_cloud-data");
-    appCloud.append(currentCloud);
+            let appCloud = document.querySelector(".app_cloud-data");
+            appCloud.append(currentCloud);
 
-    let appWind = document.querySelector(".app_wind-data");
-    appWind.append(currentWind);
+            let appWind = document.querySelector(".app_wind-data");
+            appWind.append(currentWind);
 
-    sessionStorage.setItem("name", data.location.name);
-    sessionStorage.setItem("temp", tempCurrent);
-    sessionStorage.setItem("weather", data.current.condition.text);
-    sessionStorage.setItem("icon", rightIcon);
+            sessionStorage.setItem("name", data.location.name);
+            sessionStorage.setItem("temp", tempCurrent);
+            sessionStorage.setItem("weather", data.current.condition.text);
+            sessionStorage.setItem("icon", rightIcon);
+        } else {
+            document.querySelector(".noResult").classList.remove("noneActive")
+        }
 }
 
 const btnFunc = document.querySelector(".search-btn");
